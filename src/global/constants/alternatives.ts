4e12c1479782d9f066a382467301115e9cb0861a
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-02-27 23:59:06 -0300

    feat: create constants for texts alternatives

---
+++ src/global/constants/alternatives.ts
@@ -0,0 +1,13 @@
+export const ALTERNATIVE_TEXTS = {
+  logo: 'Logo oficíal do Pokémon chamado Pokebola',
+  ash: 'Ash parado no meio do mapa, caso clique nele começara fazer movimentos como se tivesse andando',
+  pokeball: 'Imagem da Pokebola usada pelo Ash para capturar os pokemons',
+  shield:
+    'Escudo versão miniatura servindo como icone para identificar que esse campo é sobre defesa',
+  sword:
+    'Espada versão miniatura servindo como icone para identificar que esse campo é sobre ataque',
+  speed:
+    'Icone versão miniatura servindo como icone para identificar que esse campo é sobre velocidade',
+  iconClose: 'Icone em formato de X',
+  iconEdit: 'Icone com formato de um lápis.',
+}
